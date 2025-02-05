7a4da02b1df0aafb08e28eb9e8124070454bd2ce
Ritikasharma2004 <ritikasharma121104@gmail.com>
2025-02-05 12:05:17 +0530

    Create Pgm1.cs

---
+++ Pgm1.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Test
+{
+    int num;
+
+    Test(int num)
+    {
+        this.num = num;  
+    }
+
+    public static void Main()
+    {
+        Test t = new Test(4);  
+        Console.WriteLine("value is " + t.num);  
+    }
+}
