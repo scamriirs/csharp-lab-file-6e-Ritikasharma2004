da9965287fa24cee27e40538843b885813ba2a90
Ritikasharma2004 <ritikasharma121104@gmail.com>
2025-02-05 12:12:03 +0530

    Create Pgm2.cs

---
+++ Pgm2.cs
@@ -0,0 +1,21 @@
+// create a program to show the usage of parameterised constructor,parameterless constructor, default constructor
+using System;
+
+class Example
+{
+    static Example()
+    {
+        Console.WriteLine("static constructor");
+    }
+
+    Example()
+    {
+        Console.WriteLine("non-static constructor");
+    }
+
+    public static void Main()
+    {
+        Example e1 = new Example();  
+        Example e2 = new Example();  
+    }
+}
