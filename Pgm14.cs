6e47455e7ac421f07ed0183801e8c8a28b6efbc7
Ritikasharma2004 <ritikasharma121104@gmail.com>
2025-02-17 09:57:19 +0530

    Create Pgm14.cs

---
+++ Pgm14.cs
@@ -0,0 +1,5 @@
+// write a program for the seat booking management system using 2d array menu driven
+// 1- view the seats
+// 2- book the seat(provide row and column no)
+// 3- cancel the seat (provide row and column no)
+// 4- check the availabilty of perticular seat
